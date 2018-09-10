3c44978bc4f5fb5b9f4636a79be527bc72d33de6
Paul Melnikow <email@paulmelnikow.com>
2018-09-10 17:33:52 -0400

    RELEASE 20.0.post1

--- executor/__init__.py
+++ executor/__init__.py
@@ -68,7 +68,7 @@ from six import string_types, text_type
 from executor.process import ControllableProcess
 
 # Semi-standard module versioning.
-__version__ = '20.0'
+__version__ = '20.0.post1'
 
 # Initialize a logger.
 logger = logging.getLogger(__name__)
